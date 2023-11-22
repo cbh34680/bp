bf7247c18e32544d29a7b69c8a49cd847cccf6ce
cbh34680 <cbh34680@iret.co.jp>
2023-11-23 00:15:52 +0900

    test

--- elf-part.h
+++ elf-part.h
@@ -1,6 +1,3 @@
 
-typedef signed long long mem[10][20];
-
-struct {
-    char a[100];
-};
+typedef signed char __int8_t;
+#typedef __int8_t int8_t;
--- src/inc2json.py
+++ src/inc2json.py
@@ -8,61 +8,88 @@ import lark
 # https://github.com/ligurio/lark-grammars
 
 
+def types2format(types, num):
+    format = None
+
+    if num > 0:
+        if 'char' in types:
+            if 'unsigned' in types:
+                format = 'b'
+            else:
+                if num == 1:
+                    format = 'c'
+                else:
+                    format = 's'
+
+        elif 'short' in types:
+            format = 'h'
+
+        elif 'float' in types:
+            format = 'f'
+
+        elif 'double' in types:
+            format = 'd'
+
+        else:
+            xs = list(filter(lambda x: x == 'long', types))
+
+            match len(xs):
+                case 0:
+                    format = 'i'
+                case 1:
+                    format = 'l'
+                case _:
+                    format = 'q'
+
+    assert format is not None
+    return format
+
+
 #@lark.v_args(inline=True)
 class MyTrans(lark.Transformer):
 
     def __init__(self):
-        self.decls = {}
+        self.decls = []
         self.cur = {}
+        self.aliases = {}
+
+
+    def nativetypes(self, items):
+        for item in items:
+            if item.data in ('char', 'int', 'short', 'long', 'float', 'double', 'signed', 'unsigned'):
+                self.cur.setdefault('types', [])
+                self.cur['types'].append(item.data)
+
+
+    @lark.v_args(inline=True)
+    def declname(self, token):
+        self.cur['name'] = token.value
+
+
+    def declarrs(self, items):
+        for item in items:
+            self.cur.setdefault('num', 1)
+            self.cur['num'] *= int(item.value)
+
+
+    def resolve_alias(self, name, num=1):
+        if name in self.aliases:
+            alias = self.aliases[name]
+
+            self.resolve_alias()
+
         pass
 
 
-    @staticmethod
-    def types2format(types, num):
-        format = None
-
-        if num > 0:
-            if 'char' in types:
-                if 'unsigned' in types:
-                    format = 'b'
-                else:
-                    if num == 1:
-                        format = 'c'
-                    else:
-                        format = 's'
-
-            elif 'short' in types:
-                format = 'h'
-
-            elif 'float' in types:
-                format = 'f'
-
-            elif 'double' in types:
-                format = 'd'
-
-            else:
-                xs = list(filter(lambda x: x == 'long', types))
-
-                match len(xs):
-                    case 0:
-                        format = 'i'
-                    case 1:
-                        format = 'l'
-                    case _:
-                        format = 'q'
-
-        assert format is not None
-        return format, num
-
-
-    def decl(self, items):
+    def native(self, _):
         self.cur.setdefault('num', 1)
         num = self.cur['num']
 
         if num == 0:
             return
 
-        format, num = self.__class__.types2format(self.cur['types'], num)
+        types = self.cur['types']
+        format = types2format(types, num)
 
         '''
         {
@@ -84,22 +111,10 @@ class MyTrans(lark.Transformer):
         self.cur = {}
 
 
-    def decltypes(self, items):
-        for item in items:
-            if item.data in ('char', 'int', 'short', 'long', 'float', 'double', 'signed', 'unsigned'):
-                self.cur.setdefault('types', [])
-                self.cur['types'].append(item.data)
+    def typedef(self, _):
+        for decl in self.decls:
+            self.aliases[decl['name']] = decl
 
-
-    @lark.v_args(inline=True)
-    def declname(self, token):
-        self.cur['name'] = token.value
-
-
-    def declarrs(self, items):
-        for item in items:
-            self.cur.setdefault('num', 1)
-            self.cur['num'] *= int(item.value)
 
 
 def main():
