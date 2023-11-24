c2eb7b81595fd9ff6d2dbed432639a53ff6a3734
cbh34680 <cbh34680@iret.co.jp>
2023-11-24 15:03:41 +0900

    test

--- src/inc2json.py
+++ src/inc2json.py
@@ -3,6 +3,7 @@ import lark
 import pprint
 import operator
 import functools
+import collections.abc
 
 # https://zenn.dev/tbsten/articles/d922514e548518
 # https://developers.10antz.co.jp/archives/2007
@@ -11,72 +12,152 @@ import functools
 # https://github.com/ligurio/lark-grammars
 
 
+pp = pprint.PrettyPrinter(width=110)
+
 def types2format(types, num):
     format = None
 
-    if num > 0:
-        if 'char' in types:
-            if 'unsigned' in types:
-                format = 'b'
-            else:
-                if num == 1:
-                    format = 'c'
-                else:
-                    format = 's'
+    natives = tuple(filter(lambda x: x not in ('const', 'ptr'), types))
 
-        elif 'short' in types:
-            format = 'h'
-
-        elif 'float' in types:
-            format = 'f'
-
-        elif 'double' in types:
-            format = 'd'
-
+    if 'char' in natives:
+        if 'unsigned' in natives:
+            format = 'B'
         else:
-            xs = list(filter(lambda x: x == 'long', types))
+            if num == 1:
+                format = 'c'
+            else:
+                format = 's'
 
-            match len(xs):
-                case 0:
-                    format = 'i'
-                case 1:
-                    format = 'l'
-                case _:
-                    format = 'q'
+    elif 'short' in natives:
+        format = 'H' if 'unsigned' in natives else 'h'
+
+    elif 'float' in natives:
+        format = 'f'
+
+    elif 'double' in natives:
+        format = 'd'
+
+    else:
+        xs = tuple(filter(lambda x: x == 'long', natives))
+
+        match len(xs):
+            case 0:
+                format = 'I' if 'unsigned' in natives else 'i'
+            case 1:
+                format = 'L' if 'unsigned' in natives else 'l'
+            case _:
+                format = 'Q' if 'unsigned' in natives else 'q'
 
     assert format is not None
+
+    if 'ptr' in types:
+        format = 'P'
+
     return format
+
+
+def gen_decl(name, type, length, memo=None):
+
+    if memo is None:
+        memo = ''
+
+    return {
+        'name': name,
+        'type': type,
+        'length': length,
+        'memo': memo,
+    }
+
+
+def head_text(tree:lark.Tree):
+    return head_val(tree, '')
+
+def head_val(tree:lark.Tree, defval):
+    return tree.children[0].value if len(tree.children) > 0 else defval
 
 
 #@lark.v_args(inline=True)
 class MyTrans(lark.Transformer):
-
     def __init__(self):
-        pass
+        self.db = {
+            'typedef': {},
+            'struct': {},
+        }
 
 
+    # typedef
     @lark.v_args(inline=True)
-    def usertype(self, tagname, decls, typename):
+    def typedef(self, tree):
+
+        for decl in tree.children:
+            name = decl['name']
+
+            assert name != '', 'ERROR: name is empty'
+            assert name not in self.db['typedef'], f'ERROR: {name} is not in db'
+
+            self.db['typedef'][name] = decl
+
         pass
 
 
+    # struct {}
+    @lark.v_args(inline=True)
+    def usertype(self, tagname, decls, typename, num):
+
+        '''
+        https://www.tutorialspoint.com/How-to-join-list-of-lists-in-python
+
+        # input list of lists (nested list)
+        input_list = [["tutorialspoint", "python"], [2, 6, 7], [9, 5, 12, 7]]
+        print(input_list)
+
+        # Getting each element from nested Lists and storing them in a new list using list comprehension
+        resultList = [element for nestedlist in input_list for element in nestedlist]
+
+        # printing the resultant list after joining the list of lists
+        print("Resultant list after joining list of lists = ", resultList)
+        '''
+        type = tuple([ x for decl in decls.children for x in decl.children ])
+        #assert isinstance(type, collections.abc.Iterable)
+
+        tagname = head_text(tagname)
+        name = head_text(typename)
+
+        memo = f'(U)t={tagname},n={name},m=' + f'{len(type)}'
+
+        decl = gen_decl(name, type, num, memo)
+        if tagname != '':
+            self.db['struct'][tagname] = decl
+
+        return decl
 
 
+    # __int64_t, ...
+    @lark.v_args(inline=True)
+    def alias(self, orgname, name, num):
+
+        org = self.db['typedef'][orgname]
+
+        num *= org['length']
+        memo = org['memo'] + f', (A){orgname}'
+
+        return gen_decl(name.value, org['type'], num, memo)
+
+
+    # char, int, ...
     @lark.v_args(inline=True)
     def stdtype(self, types, name, num):
 
         format = types2format(types, num)
+        memo = '(S)' + ' '.join(types)
 
-        return {
-            'name': name.value,
-            'type': format,
-            'length': num,
-        }
+        return gen_decl(name.value, format, num, memo)
 
     def builtins(self, items):
-        return list(map(lambda x: x.data, items))
+        return tuple(map(lambda x: x.data, items))
 
 
+    # [10]
     def declarrs(self, items):
         if len(items) == 0:
             return 1
@@ -106,10 +187,7 @@ def main():
         trans = MyTrans()
         trans.transform(tree)
 
-        pprint.pprint(trans.__dict__)
-        pass
-
-    pass
+        pp.pprint(trans.__dict__)
 
 
 if __name__ == '__main__':
