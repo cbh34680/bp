b24e5c3f36d397bfe20cdbfcd9e2f1ac4aad9b7a
cbh34680 <cbh34680@iret.co.jp>
2023-11-25 09:44:17 +0900

    test

--- ltest.h
+++ ltest.h
@@ -1,6 +1,10 @@
 
 #define SIZE7 (2 + 5)
 #define SIZE SIZE7
+#define SIZE100 ((SIZE + 3) * 10)
 
 typedef unsigned char mem96[10 - SIZE + 1];
 typedef char mem10[10];
+typedef mem10 mem100[10];
+typedef mem100 mem1000[10];
+typedef mem10 mem2000[SIZE100][2];
